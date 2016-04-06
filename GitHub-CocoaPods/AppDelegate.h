c164e5193998d057e4c53838b364d686bc8fa8f9
ming <1079278593@qq.com>
2016-04-06 09:32:09 +0800

    a project test CocoaPods for CI

---
+++ GitHub-CocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHub-CocoaPods
+//
+//  Created by ming on 16/4/6.
+//  Copyright © 2016年 ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHub-CocoaPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub-CocoaPods
+//
+//  Created by ming on 16/4/6.
+//  Copyright © 2016年 ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
