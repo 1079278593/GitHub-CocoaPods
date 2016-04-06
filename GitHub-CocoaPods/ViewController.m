2a19ae8732dc8b8963e6638e992fc8d076ed5ca6
ming <1079278593@qq.com>
2016-04-06 10:23:21 +0800

    add some change

--- GitHub-CocoaPods/ViewController.m
+++ GitHub-CocoaPods/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#define KMainScreenWidth [UIScreen mainScreen].bounds.size.width
+#define KMainScreenHeight [UIScreen mainScreen].bounds.size.height
 
 @interface ViewController ()
 
@@ -17,6 +19,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake((KMainScreenWidth - 80)/2.0, 30, 80, 50);
+    [button setTitle:@"click" forState:UIControlStateNormal];
+    [self.view addSubview:button];
+    button.backgroundColor = [UIColor lightGrayColor];
+    [button addTarget:self action:@selector(btnAct:) forControlEvents:UIControlEventTouchUpInside];
+    
+}
+
+-(void)btnAct:(UIButton *)button{
+    
+    UIImageView *normalImage = [[UIImageView alloc]init];
+    normalImage.frame = CGRectMake((KMainScreenWidth - 250)/2.0, 100, 250, 300);
+    normalImage.contentMode = UIViewContentModeScaleAspectFit;
+    //    normalImage.backgroundColor = [UIColor colorWithRed:.1 green:.1 blue:.1 alpha:.13];
+    normalImage.image = [UIImage imageNamed:@"mayun.png"];
+    [self.view addSubview:normalImage];
+    
 }
 
 - (void)didReceiveMemoryWarning {
